88c278706a26413ba44ab4e3e904380e7251a579
macabrekarl <gwadalitokarl@gmail.com>
2018-01-18 14:14:42 -0400

    header fini

---
+++ src/Components/Accueil.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+
+class Accueil extends Component {
+    render() {
+        return (
+            <div className="TestAccueil">
+                <h1>test</h1>
+            </div>
+        );
+    }
+}
+
+export default Accueil;
